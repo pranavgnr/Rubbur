bfd63a23977469a4b342378e42db5592da997770
pranavgnr <gnrpranav@gmail.com>
2024-06-16 00:17:45 +0530

    set few color themes and components and main-screen and card.

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { SharedModule } from '../shared/shared.module';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-  
-    MainScreenComponent
+    MainScreenComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,

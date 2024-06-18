e692e153c30e401dedeeb3a31524187754a0a24c
pranavgnr <gnrpranav@gmail.com>
2024-06-18 22:51:25 +0530

    ngx-editor implemented.

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { SharedModule } from '../shared/shared.module';
 import { CardComponent } from './main-screen/card/card.component';
+import { NgxEditorModule } from 'ngx-editor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,56 @@ import { CardComponent } from './main-screen/card/card.component';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    NgxEditorModule.forRoot({
+      locals: {
+        // menu
+        bold: 'Bold',
+        italic: 'Italic',
+        code: 'Code',
+        underline: 'Underline',
+        strike: 'Strike',
+        blockquote: 'Blockquote',
+        bullet_list: 'Bullet List',
+        ordered_list: 'Ordered List',
+        heading: 'Heading',
+        h1: 'Header 1',
+        h2: 'Header 2',
+        h3: 'Header 3',
+        h4: 'Header 4',
+        h5: 'Header 5',
+        h6: 'Header 6',
+        align_left: 'Left Align',
+        align_center: 'Center Align',
+        align_right: 'Right Align',
+        align_justify: 'Justify',
+        text_color: 'Text Color',
+        background_color: 'Background Color',
+        horizontal_rule: 'Horizontal rule',
+        format_clear: 'Clear Formatting',
+        insertLink: 'Insert Link',
+        removeLink: 'Remove Link',
+        insertImage: 'Insert Image',
+        indent: 'Increase Indent',
+        outdent: 'Decrease Indent',
+        superscript: 'Superscript',
+        subscript: 'Subscript',
+        undo: 'Undo',
+        redo: 'Redo',
+    
+        // pupups, forms, others...
+        url: 'URL',
+        text: 'Text',
+        openInNewTab: 'Open in new tab',
+        insert: 'Insert',
+        altText: 'Alt Text',
+        title: 'Title',
+        remove: 'Remove',
+        enterValidUrl: 'Please enter a valid URL',
+      },
+    }),
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class FeaturesModule { }
--- src/app/features/main-screen/card/card.component.ts
+++ src/app/features/main-screen/card/card.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 import { FeaturesService } from '../../features.service';
+import { Editor, Toolbar, Validators } from 'ngx-editor';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
@@ -8,19 +10,50 @@ import { FeaturesService } from '../../features.service';
 })
 export class CardComponent {
   @Input() data!: string;
+  @Input() cardExpanded!: boolean;
   @Output() buttonClicked = new EventEmitter<string>();
 
-  title: string = ''
+  title: string = '';
+  editor!: Editor;
+  html: any = ''
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
 
   constructor(private featureService: FeaturesService) {}
 
   ngOnInit() {
-    this.title = (this.data.split(' '))[0]
+    this.title = (this.data.split(' '))[0];
+    this.editor = new Editor();
+  }
+
+  form = new FormGroup({
+    editorContent: new FormControl('', Validators.required()),
+  });
+
+  onChange(words: object) {
+    this.html = words;
   }
 
   onNewBranchClicked() {
-    console.log(this.title)
-    this.buttonClicked.emit(this.title)
+    // console.log(this.title)
+    // this.buttonClicked.emit(this.title)
+  }
+
+  // make sure to destory the editor
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+
+  saveData() {
+    this.data = this.html
   }
   
 }
